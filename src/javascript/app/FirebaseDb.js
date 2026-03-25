3d761478695d4d2b0c82625b31b62cb0301ed0e7
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-03-25 20:01:00 +0000

    Added FirebaseDb.js, including basic methods to implement.

---
+++ src/javascript/app/FirebaseDb.js
@@ -0,0 +1,21 @@
+class FirebaseDb {
+    /* TODO: Implementar metodos, diseñar base de datos y establecer input de funciones*/
+    constructor() {
+        /* Abrir conexión a base de datos */
+    }
+
+    makeReservation() {
+    }
+
+    cancelReservation() {
+    }
+
+    findGymsByDistance() {
+
+    }
+
+    addGym() {
+
+    }
+
+}
