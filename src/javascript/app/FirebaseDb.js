ab84f76c5f3e94c5dc8fc728fba238ac8f17cec1
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-03-29 12:12:04 +0100

    Fixed firebase configuration leak. Firebase config now appears in another file.

--- src/javascript/app/FirebaseDb.js
+++ src/javascript/app/FirebaseDb.js
@@ -7,15 +7,7 @@ class FirebaseDb {
 
     constructor() {
         /* Abrir conexión a base de datos */
-        const firebaseConfig = {
-            apiKey: "AIzaSyCnZJMsMzZ1CZfJNfjbhBZ9x_xA9tQcBjg",
-            authDomain: "proyecto2026ps.firebaseapp.com",
-            projectId: "proyecto2026ps",
-            storageBucket: "proyecto2026ps.firebasestorage.app",
-            messagingSenderId: "124816158835",
-            appId: "1:124816158835:web:39a68c1772fa3427a33a06",
-            measurementId: "G-0G1XLG4TN2"
-        }
+        const firebaseConfig = fetch("../assets/firebaseConfig.json").then((r) => r.json());
         firebase.initializeApp()
         this.db = firebase.firestore()
     }
