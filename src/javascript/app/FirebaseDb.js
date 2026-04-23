6481095226f9168afa09757d7c9b079fb3dd0203
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-04-23 16:15:48 +0100

    Refactored History page (unfinished), moved Profile user page script to its own JavaScript file

--- src/javascript/app/FirebaseDb.js
+++ src/javascript/app/FirebaseDb.js
@@ -12,7 +12,8 @@ import {
     where,
     updateDoc,
     serverTimestamp,
-    setDoc
+    setDoc,
+    runTransaction
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
 
 const firebaseConfig = await fetch('../assets/firebaseConfig.json');
@@ -191,6 +192,26 @@ export default class FirebaseDb {
         }
     }
 
+    async getGym(gymId) {
+        const gymRef = await doc(this.db, "gyms", gymId);
+        if (gymRef) {
+            const gymDoc = await getDoc(gymRef);
+            return gymDoc.data()
+        } else {
+            return {};
+        }
+    }
+
+    async getProfessional(proId) {
+        const proRef = await doc(this.db, "gyms", proId);
+        if (proRef) {
+            const proDoc = await getDoc(proRef);
+            return proDoc.data()
+        } else {
+            return {};
+        }
+    }
+
     async addMaterial(materialData) {
         const materialsRef = collection(this.db, "materials");
         const docRef = await addDoc(materialsRef, {
@@ -210,6 +231,16 @@ export default class FirebaseDb {
             createdAt: serverTimestamp()
         });
         return docRef.id;
+    }
+
+    async getActivity(activityId) {
+        const activityRef = await doc(this.db, "activities", activityId);
+        if (activityRef) {
+            const activityDoc = await getDoc(activityRef);
+            return activityDoc.data()
+        } else {
+            return {};
+        }
     }
 
     async getUserReservations(userId, status = null) {
@@ -240,6 +271,9 @@ export default class FirebaseDb {
     }
 
     async rateTarget(targetId, targetType, score) {
+        if (targetType !== "gym" && targetType !== "professional") {
+            throw new Error("targetType must be a gym or professional");
+        }
         const colName = targetType === "gym" ? "gyms" : "professionals";
         const targetRef = doc(this.db, colName, targetId);
         const targetSnap = await getDoc(targetRef);
